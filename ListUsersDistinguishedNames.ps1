3a63c3f5e07d961b8944479649f0038b92d76260
Cimitra Software GitHub Repository <55113746+cimitrasoftware@users.noreply.github.com>
2020-07-15 23:18:00 -0600

    Add files via upload

---
+++ ListUsersDistinguishedNames.ps1
@@ -0,0 +1,46 @@
+﻿function  Get-DistinguishedName {
+    param (
+        [Parameter(Mandatory,
+        ParameterSetName = 'Input')]
+        [string[]]
+        $CanonicalName,
+
+        [Parameter(Mandatory,
+            ValueFromPipeline,
+            ParameterSetName = 'Pipeline')]
+        [string]
+        $InputObject
+    )
+    process {
+        if ($PSCmdlet.ParameterSetName -eq 'Pipeline') {
+            $arr = $_ -split '/'
+            [array]::reverse($arr)
+            $output = @()
+            $output += $arr[0] -replace '^.*$', 'CN=$0'
+            $output += ($arr | select -Skip 1 | select -SkipLast 1) -replace '^.*$', 'OU=$0'
+            $output += ($arr | ? { $_ -like '*.*' }) -split '\.' -replace '^.*$', 'DC=$0'
+            $output -join ','
+        }
+        else {
+            foreach ($cn in $CanonicalName) {
+                $arr = $cn -split '/'
+                [array]::reverse($arr)
+                $output = @()
+                $output += $arr[0] -replace '^.*$', 'CN=$0'
+                $output += ($arr | select -Skip 1 | select -SkipLast 1) -replace '^.*$', 'OU=$0'
+                $output += ($arr | ? { $_ -like '*.*' }) -split '\.' -replace '^.*$', 'DC=$0'
+                $output -join ','
+            }
+        }
+    }
+}
+
+
+
+Write-Output ""
+Write-Output "------------------------------------------------------------------"
+Write-Output ""
+@(Get-ADUser -Filter *) | Get-DistinguishedName
+Write-Output ""
+Write-Output "------------------------------------------------------------------"
+
