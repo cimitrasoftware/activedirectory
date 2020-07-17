799fe12f47c484d72558b4b15db64d3553297a10
Cimitra Software GitHub Repository <55113746+cimitrasoftware@users.noreply.github.com>
2020-07-17 10:58:28 -0600

    Add files via upload

---
+++ NewUserNew.ps1
@@ -0,0 +1,105 @@
+﻿# Create a New User in Active Directory
+# Author: Tay Kratzer tay@cimitra.com
+# Change the context variable to match your system
+# -------------------------------------------------
+$context = "OU=USERS,OU=DEMO,OU=CIMITRA,DC=cimitrademo,DC=com" 
+ # - OR -
+ # Specify the context in settings.cfg file
+ # Use this format: AD_USER_CONTEXT=<ACTIVE DIRECTORY CONTEXT>
+ # Example: AD_USER_CONTEXT=OU=COMPUTERS,OU=DEMO,OU=CIMITRA,DC=cimitrademo,DC=com
+ # -------------------------------------------------
+
+# Look to see if a config_reader.ps1 file exists in order to use it's functionality
+if((Test-Path ${PSScriptRoot}\config_reader.ps1)){
+
+# If a settings.cfg file exists, let's use that file to reading in variables
+if((Test-Path ${PSScriptRoot}\settings.cfg))
+{
+# Give a short name to the config_reader.ps1 script
+$CONFIG_IO="${PSScriptRoot}\config_reader.ps1"
+
+# Source in the configuration reader script
+. $CONFIG_IO
+
+# Use the "ReadFromConfigFile" function in the configuration reader script
+$CONFIG=(ReadFromConfigFile "${PSScriptRoot}\settings.cfg")
+
+# Map the $context variable to the AD_USER_CONTEXT variable read in from the settings.cfg file
+$context = "$CONFIG$AD_USER_CONTEXT"
+}
+
+}
+
+
+# Show Help
+function ShowHelp{
+$scriptName = Split-Path -leaf $PSCommandpath
+Write-Host ""
+Write-Host "Script Usage"
+Write-Host ""
+Write-Host ".\$scriptName <user first name> <user last name> <password> <Active Directory context (optional if specified otherwise)>"
+Write-Host ""
+Write-Host "Example: .\$scriptName Jamie Smith changeMe"
+Write-Host ""
+Write-Host "-OR-"
+Write-Host ""
+Write-Host "Example: .\$scriptName Jamie Smith changeMe OU=USERS,OU=DEMO,OU=CIMITRA,DC=cimitrademo,DC=com"
+Write-Host ""
+exit 0
+}
+
+# This script expects 3 arguments, so if the 3rd argument is blank, then show the Help and exit
+if (!$args[2]){ 
+ShowHelp
+ }
+# -------------------------------------------------
+
+
+# If a fourth argument is sent into this script, that fourth argument will be mapped to the $context variable
+
+if ($args[3]) { 
+
+$context = $args[3]
+
+}
+
+# Get the first command-line variable
+$firstNameIn=$args[0]
+# Make sure the first character is uppercased
+$firstNameIn = (Get-Culture).TextInfo.ToTitleCase($firstNameIn)
+# Get the second command-line variable
+$lastNameIn=$args[1]
+# Make sure the first character is uppercased
+$lastNameIn = (Get-Culture).TextInfo.ToTitleCase($lastNameIn)
+# Get the third command-line variable
+$passwordIn=$args[2]
+# Make the samAccountName variable from a combination of the user's first and last name
+$samAccountName = ($firstNameIn+$lastNameIn).ToLower()
+
+
+# Create the new user
+New-ADUser -Name "$firstNameIn $lastNameIn" -GivenName "$firstNameIn" -Surname "$lastNameIn" -SamAccountName "$samAccountName" -AccountPassword (ConvertTo-SecureString "$passwordIn" -AsPlainText -force) -passThru -path "$context"
+
+# Catch the exit code from running the command
+$theResult = $?
+
+# If exit code from the New-ADUser command was "True" then show a success message
+if ($theResult)
+{
+Write-Output ""
+Write-Output ""
+Write-Output "New User ${firstNameIn} ${lastNameIn} created in Active Directory"
+Write-Output ""
+}else{
+Write-Output ""
+Write-Output ""
+Write-Output "User ${firstNameIn} ${lastNameIn} NOT created in Active Directory"
+Write-Output ""
+}
+
+# Enable the account
+Enable-ADAccount -Identity "CN=$firstNameIn $lastNameIn,$context" -Confirm:$False
+
+# Force an immediate password reset
+Set-ADUser -Identity  "CN=$firstNameIn $lastNameIn,$context" -ChangePasswordAtLogon $true
+
