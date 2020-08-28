2c14d20ca02edef2a7b3178ab991876f8356bbe3
Cimitra Software GitHub Repository <55113746+cimitrasoftware@users.noreply.github.com>
2020-08-28 14:36:51 -0700

    Add files via upload

---
+++ ad.ps1
@@ -0,0 +1,11 @@
+﻿# Cimitra Windows Active Directory Setup Stub Script
+# Version 1.0
+# Author: Tay Kratzer tay@cimitra.com
+# --------------------------------------------------
+
+$CURRENT_PATH = $PWD.Path
+write-output ""
+write-output "START: RUNNING - Cimitra Active Directory Integration Setup Script"
+write-output "------------------------------------------------------------------"
+Write-Output ""
+& "$CURRENT_PATH\setup.ps1"
